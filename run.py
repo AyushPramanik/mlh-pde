d9e1561afea84c21f066bf84e397e1f11266335a
Adhar Chaudhari <chaudhari.adhar@outlook.com>
2026-04-05 09:31:30 +0530

    port fixing

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ app = create_app()
 
 if __name__ == "__main__":
     host = os.environ.get("FLASK_HOST", "127.0.0.1")
-    port = int(os.environ.get("FLASK_PORT", 5000))
+    port = int(os.environ.get("PORT") or os.environ.get("FLASK_PORT") or 5000)
     debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
 
     # Retry DB connection on startup
